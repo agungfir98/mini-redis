b841884dc3860f04c9d83ba1dac61b7068c6da11
agungfir98 <agungfir98@gmail.com>
2025-07-24 12:18:40 +0700

    refactor: test with more clear flow

--- handler/message-handler_test.go
+++ handler/message-handler_test.go
@@ -7,24 +7,61 @@ import (
 )
 
 func TestMessageHandler(t *testing.T) {
-	testSetGetDel(t)
+	testStringData(t) // get set del ....
 }
 
 type SetTestCase struct {
-	name string
-	args []proto.RespMessage
-	want proto.RespMessage
+	name    string
+	setArgs []proto.RespMessage
+	getArgs []proto.RespMessage
+	delArgs []proto.RespMessage
+	setWant proto.RespMessage
+	getWant proto.RespMessage
+	delWant proto.RespMessage
 }
 
-func testSetGetDel(t *testing.T) {
+func testStringData(t *testing.T) {
 	tc := []SetTestCase{
-		{name: "set foo bar", args: []proto.RespMessage{{Typ: "string", String: "SET"}, {Typ: "string", String: "foo"}, {Typ: "string", String: "bar"}}, want: proto.RespMessage{Typ: "status", Status: "OK"}},
+		{
+			name: "String command",
+			setArgs: []proto.RespMessage{
+				{Typ: "string", String: "SET"},
+				{Typ: "string", String: "foo"},
+				{Typ: "string", String: "bar"},
+			},
+			getArgs: []proto.RespMessage{
+				{Typ: "string", String: "GET"},
+				{Typ: "string", String: "foo"},
+			},
+			delArgs: []proto.RespMessage{
+				{Typ: "string", String: "DEL"},
+				{Typ: "string", String: "foo"},
+			},
+			setWant: proto.RespMessage{Typ: "status", Status: "OK"},
+			getWant: proto.RespMessage{Typ: "string", String: "bar"},
+			delWant: proto.RespMessage{Typ: "integer", Num: 1},
+		},
 	}
 
-	// TODO:
-	// This testing flow is awful, gotta refactor sometimes later
-	t.Run("test set get del", func(t *testing.T) {
-		testSet(t, tc)
-		testGet(t, tc)
+	for _, c := range tc {
+		t.Run(c.name, func(t *testing.T) {
+			var skip bool
+			runStep(t, "set", &skip, func(t *testing.T) { testSet(t, c) })
+			runStep(t, "get", &skip, func(t *testing.T) { testGet(t, c) })
+			runStep(t, "del", &skip, func(t *testing.T) { testDel(t, c) })
+		})
+	}
+}
+
+func runStep(t *testing.T, name string, skip *bool, fn func(t *testing.T)) {
+	t.Run(name, func(t *testing.T) {
+		if *skip {
+			t.Skipf("skipping %s because earlier step failed", name)
+		}
+
+		fn(t)
+		if t.Failed() {
+			*skip = true
+		}
 	})
 }
