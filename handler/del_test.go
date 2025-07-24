fa12caefb767c14771832972609f4024825f2a17
agungfir98 <agungfir98@gmail.com>
2025-07-24 12:16:06 +0700

    add test for string del command

---
+++ handler/del_test.go
@@ -0,0 +1,21 @@
+package handler
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func testDel(t *testing.T, c SetTestCase) {
+	cmd := strings.ToUpper(c.delArgs[0].String)
+	args := c.delArgs[1:]
+	handler, ok := Message[cmd]
+	if !ok {
+		t.Fatalf("no such command %v, test: %v\n", cmd, c.name)
+	}
+
+	got := handler(args)
+	if !reflect.DeepEqual(c.delWant, got) {
+		t.Fatalf("expected: %v, got: %v\n", c.delWant, got)
+	}
+}
